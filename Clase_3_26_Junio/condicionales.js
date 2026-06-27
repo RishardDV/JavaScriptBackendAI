e65bdb3ca0411869dce62ae81be2c57d0197d872
RishardDV <richardmontessha@gmail.com>
2026-06-26 21:01:19 -0500

    Contenido de la clase 3

---
+++ Clase_3_26_Junio/condicionales.js
@@ -0,0 +1,53 @@
+//If basico
+// 
+/*
+
+const prioridad = 4;
+
+
+if (prioridad >= 5) {
+
+  console.log("Esta tarea es URGENTE");
+
+} 
+
+//If con else
+const prioridad = 4;
+
+
+if (prioridad >= 5) {
+
+  console.log("Esta tarea es URGENTE");
+} else{
+    console.log("Normal");
+}
+
+
+//If con niveles
+const prioridad = 4;
+
+if (prioridad >= 5) {
+
+  console.log("Esta tarea es URGENTE");
+
+} else if (prioridad >= 3) {
+    console.log("Alta");
+} else{
+    console.log("Baja");
+}
+
+const titulo = "";
+
+ */
+
+const titulo ="0";
+
+if (titulo) {
+
+  console.log("La tarea tiene título");
+
+} else {
+
+  console.log("Falta el título");   // ← entra aquí: "" es falsy
+
+};
---
+++ Clase_3_26_Junio/for.js
@@ -0,0 +1,14 @@
+//Ciclo for
+
+for (let i = 0; i < 3; i++) {
+
+  console.log("Vuelta número " + i);
+
+}
+
+//Ciclo for of
+const tareas = ["Pagar factura", "Regar plantas", "Llamar cliente"];
+
+for (const tarea of tareas) {
+    console.log("Tarea: " + tarea);
+}
